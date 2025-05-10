7b1adcf1473487d3edae32e38e0e13a389e18d37
Ivan <statkevich.ivan999@gmail.com>
2025-05-10 22:09:08 +0300

    Initial setup of the project structure and files.

---
+++ scripts/python/create-lxc.py
@@ -0,0 +1,26 @@
+list_of_container_settings = [
+    {
+        "name": "lxc-proxy",
+    },
+    {
+        "name": "lxc-app",
+    },
+    {
+        "name": "lxc-db",
+    },
+    {
+        "name": "lxc-monitoring",
+    },
+    {
+        "name": "lxc-forgejo",
+    },
+    {
+        "name": "lxc-drone-server",
+    },
+    {
+        "name": "lxc-drone-runner",
+    },
+    {
+        "name": "lxc-backup",
+    },
+]
