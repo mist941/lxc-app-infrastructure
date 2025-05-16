a6b474525878719560193b9d2b87ee7bb3f49761
Ivan <statkevich.ivan999@gmail.com>
2025-05-16 21:21:43 +0300

    Add Nginx proxy configurations for app, forgejo, and monitoring services in ngingx-setup.sh, including symbolic links for site enabling and firewall adjustments for improved service management.

--- ngingx-setup.sh
+++ ngingx-setup.sh
@@ -6,8 +6,59 @@ sudo apt update && sudo apt upgrade -y
 
 sudo apt install -y nginx
 
+cat <<EOF > /etc/nginx/sites-available/app-proxy
+  server {
+    listen 80;
+    server_name app.example.com;
+
+    location / {
+        proxy_pass <ip_address>;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+    }
+  }
+EOF
+
+cat <<EOF > /etc/nginx/sites-available/forgejo-proxy
+  server {
+    listen 80;
+    server_name forgejo.example.com;
+
+    location / {
+        proxy_pass <ip_address>;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+    }
+  }
+EOF
+
+cat <<EOF > /etc/nginx/sites-available/monitoring-proxy
+  server {
+    listen 80;
+    server_name monitoring.example.com;
+
+    location / {
+        proxy_pass <ip_address>;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+    }
+  }
+EOF
+
+sudo ln -s /etc/nginx/sites-available/app-proxy /etc/nginx/sites-enabled/
+sudo ln -s /etc/nginx/sites-available/forgejo-proxy /etc/nginx/sites-enabled/
+sudo ln -s /etc/nginx/sites-available/monitoring-proxy /etc/nginx/sites-enabled/
+
 sudo systemctl enable nginx
+
 sudo systemctl start nginx
 
 sudo ufw allow 'Nginx Full'
+
 sudo ufw enable
