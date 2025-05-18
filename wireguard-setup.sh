8bd981d5eb775537ede8b7f485b9a6e735328ba7
Ivan <statkevich.ivan999@gmail.com>
2025-05-18 18:13:58 +0300

    Add WireGuard configuration to wireguard-setup.sh, including key generation and interface setup for secure VPN connection.

--- wireguard-setup.sh
+++ wireguard-setup.sh
@@ -6,9 +6,21 @@ sudo apt update && sudo apt upgrade -y
 
 sudo apt install -y wireguard
 
-sudo systemctl enable wg-quick@wg0
-sudo systemctl start wg-quick@wg0
+umask 077
 
-sudo ufw allow 51820/udp
-sudo ufw enable
+wg genkey | tee /etc/wireguard/privatekey | wg pubkey > /etc/wireguard/publickey
 
+cat <<EOF > /etc/wireguard/wg0.conf
+[Interface]
+PrivateKey = $(cat /etc/wireguard/privatekey)
+Address = 10.0.0.1/24
+ListenPort = 51820
+SaveConfig = true
+
+[Peer]
+PublicKey = $(cat /etc/wireguard/publickey)
+AllowedIPs = 0.0.0.0/0
+
+PostUp = iptables -A FORWARD -i %i -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+PostDown = iptables -D FORWARD -i %i -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE
+EOF
