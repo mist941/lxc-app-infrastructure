92bb27c92733add87d198fcbc7d743e75501cd70
Ivan <statkevich.ivan999@gmail.com>
2025-05-18 18:16:57 +0300

    Add commands to wireguard-setup.sh for enabling and starting WireGuard service, and reload sysctl settings for immediate effect.

--- wireguard-setup.sh
+++ wireguard-setup.sh
@@ -24,3 +24,8 @@ AllowedIPs = 0.0.0.0/0
 PostUp = iptables -A FORWARD -i %i -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 PostDown = iptables -D FORWARD -i %i -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE
 EOF
+
+sudo sysctl -p
+
+sudo systemctl enable wg-quick@wg0
+sudo systemctl start wg-quick@wg0
